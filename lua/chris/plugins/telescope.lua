b106e3bfb72beefd086be669f698d65eeab39ab8
Chris Wang <chriseng2023@gmail.com>
2023-03-06 00:53:57 +0800

    Add icon, comment, keymaps.

--- lua/chris/plugins/telescope.lua
+++ lua/chris/plugins/telescope.lua
@@ -14,8 +14,16 @@ end
 telescope.setup({
   -- configure custom mappings
   defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+
     mappings = {
       i = {
+        ["<Down>"] = actions.cycle_history_next,
+        ["<Up>"] = actions.cycle_history_prev,
         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
         ["<C-j>"] = actions.move_selection_next, -- move to next result
         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
@@ -23,5 +31,6 @@ telescope.setup({
     },
   },
 })
-
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
 telescope.load_extension("fzf")
